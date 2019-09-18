6954a69a400b869c456715285f0cc00daeeb90fe
edwin lopez <edwingl@uci.edu>
2019-09-18 12:02:32 -0700

    Updates

---
+++ precache-manifest.3140dce096875321a60f31dd1c1ba87a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c3223e8a9584139988b2b9b04be3ec",
+    "url": "/infoweather/index.html"
+  },
+  {
+    "revision": "9b9fbeb1861274a9f0d1",
+    "url": "/infoweather/static/css/2.06959b98.chunk.css"
+  },
+  {
+    "revision": "7be150e5c54e2436738d",
+    "url": "/infoweather/static/css/main.e90fef8d.chunk.css"
+  },
+  {
+    "revision": "9b9fbeb1861274a9f0d1",
+    "url": "/infoweather/static/js/2.130996e9.chunk.js"
+  },
+  {
+    "revision": "7be150e5c54e2436738d",
+    "url": "/infoweather/static/js/main.6d76c5d1.chunk.js"
+  },
+  {
+    "revision": "05c938694da052fbad3c",
+    "url": "/infoweather/static/js/runtime~main.b5507d03.js"
+  },
+  {
+    "revision": "7cb6290c4598a7aca6ce30ddbb72633d",
+    "url": "/infoweather/static/media/cloudsBackground.7cb6290c.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/infoweather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/infoweather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/infoweather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/infoweather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/infoweather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
